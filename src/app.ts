e2b84fd8caa109694f5904442316f2081a8cb7d8
Shiv10 <shivanshsharma2012@gmail.com>
2021-08-06 12:51:53 +0530

    feat: add names and emails

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,17 @@ app.get('/', async (req: Request, res: Response)=> {
         return res.json({success: false});
     }
     const users = await User.find({});
-    res.json({success: true, count: users.length});
+    const arr: Array<Object> = []
+    if (users.length>0){
+        users.forEach((document: any) => {
+            const obj: Object = {
+                "name": document.name,
+                "email": document.email
+            } 
+            arr.push(obj)
+        });
+    }
+    res.json({success: true, count: users.length, list: arr});
     res.end();
 })
 
